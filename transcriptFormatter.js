ad78a5e29830f79a10b69928543aece04390fae7
jepajerowski <69466122+jepajerowski@users.noreply.github.com>
2023-05-19 15:03:58 -0400

    Update transcriptFormatter.js

--- transcriptFormatter.js
+++ transcriptFormatter.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('submit');
 btn.addEventListener('click', () => {
     const editorContent = tinymce.get('tiny').getContent();
-    const formattedContent = editorContent.replace(/<p[^>]+>/g, "<p>").replace("<p>TRANSCRIPT</p>",'<hr class="my-2x"/><h2>Transcript</h2>').replaceAll("<strong>",'<strong class="sans-serif font-weight-bold">').replace(/\d:\d\d:\d\d\.\d/g,'<span class="text-sm text-dark-gray">$&</span><br/>').replaceAll("<p>&nbsp;</p>","").replaceAll(/\[.+?\]/g, '<span class="sans-serif font-weight-bold text-sm letter-spacing-default text-dark-gray">$&</span>');
+    const formattedContent = editorContent.replace(/<p[^>]+>/g, "<p>").replace("<p>TRANSCRIPT</p>",'<hr class="my-2x"/><h2>Transcript</h2>').replaceAll(/<strong>|<b><span>/g,'<strong class="sans-serif font-weight-bold">').replaceAll("</span></b>","</strong>").replace(/\d:\d\d:\d\d\.\d/g,'<span class="text-sm text-dark-gray">$&</span><br/>').replaceAll("<p>&nbsp;</p>","").replaceAll(/\[.+?\]/g, '<span class="sans-serif font-weight-bold text-sm letter-spacing-default text-dark-gray">$&</span>');
     const outputElement = document.getElementById('output-content');
     outputElement.innerText = formattedContent;
     
@@ -11,4 +11,3 @@ btn.addEventListener('click', () => {
     document.getElementById('confirmation').style.opacity = "1";
 
 });
-
