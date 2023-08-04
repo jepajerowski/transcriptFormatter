70e26777fa40fec49579aa73802877c5fcaac109
jepajerowski <69466122+jepajerowski@users.noreply.github.com>
2023-08-04 12:46:09 -0400

    Adjustments to replace patterns
    Adjusted based on sample transcript provided by Kevin and Karalee

--- transcriptFormatter.js
+++ transcriptFormatter.js
@@ -1,13 +1,21 @@
+function transcriptReplace(transcript){
+    let str = transcript;
+    str = str.replace(/<p[^>]+?>/g, "<p>");
+    str = str.replace(/<p><span class="Bold">(\d:\d\d:\d\d\.\d)\s(.+?):<\/span>/g, '<div class="text-sm text-dark-gray font-weight-bold mb-1">$1</div>\n<p><span class="sans-serif font-weight-bold">$2:</span>');
+    str = str.replaceAll("<p>&nbsp;</p>","");
+    str = str.replaceAll(/\[.+?\]/g, '<span class="sans-serif font-weight-bold text-sm letter-spacing-default text-dark-gray">$&</span>');
+    h2 = '<hr class="my-2x"/><h2>Transcript</h2>';
+    str = h2.concat("\n", str);
+    return str;
+}
+
 const btn = document.getElementById('submit');
 btn.addEventListener('click', () => {
-    const editorContent = tinymce.get('tiny').getContent();
-    const formattedContent = editorContent.replace(/<p[^>]+>/g, "<p>").replace("<p>TRANSCRIPT</p>",'<hr class="my-2x"/><h2>Transcript</h2>').replaceAll(/<strong>|<b><span>/g,'<strong class="sans-serif font-weight-bold">').replaceAll("</span></b>","</strong>").replace(/\d:\d\d:\d\d\.\d/g,'<span class="text-sm text-dark-gray">$&</span><br/>').replaceAll("<p>&nbsp;</p>","").replaceAll(/\[.+?\]/g, '<span class="sans-serif font-weight-bold text-sm letter-spacing-default text-dark-gray">$&</span>');
+    let transcript = tinymce.get('tiny').getContent();
+    transcript = transcriptReplace(transcript);
+
     const outputElement = document.getElementById('output-content');
-    outputElement.innerText = formattedContent;
-    
-
-
-    navigator.clipboard.writeText(formattedContent);
+    outputElement.innerText = transcript;
+    navigator.clipboard.writeText(transcript);
     document.getElementById('confirmation').style.opacity = "1";
-
 });
